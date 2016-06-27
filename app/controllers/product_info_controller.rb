4428bb092de1032ba7b2a71070c7c471736cea86
snlamm <shmuel.lamm@gmail.com>
2016-06-27 18:40:37 -0400

    add nokogiri. in progress.

--- app/controllers/product_info_controller.rb
+++ app/controllers/product_info_controller.rb
@@ -1,7 +1,9 @@
+require 'open-uri'
+
 class ProductInfoController < ApplicationController
   def index
     amazon_url = Adapter::Amazon.new("Fashion", "corderoy pants")
-    
     binding.pry
+    response =  Nokogiri::HTML(open(amazon_url.signed_url))
   end
 end
